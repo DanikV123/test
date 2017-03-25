2f44109fe4e5f48418f19668320e86c8f3700d1a
danil <danilavajda@gmail.com>
2017-03-25 22:47:56 +0200

    Schema added

--- server.js
+++ server.js
@@ -1,52 +1,72 @@
-/**
- * Created by Larisa on 10.02.2017.
- */
 var express = require('express');
 var app = express();
-var port;
+var port = process.env.PORT || 3030;;
 var mongoose = require ('mongoose');
 var conection;
-var conection2;
+/*var conectin1;
+var conection2;*/
 
+//conection = mongoose.createConnection(process.env.DB_HOST, parseInt(process.env.DB_PORT), process.env.DB_NAME);
+//conection2 = mongoose.createConnection(process.env.DB_HOST, parseInt(process.env.DB_PORT), 'jspro');
 //mongoose.connect('localhost', 27017, 'test');
-
-require('./config/development');
 //mongoose.connect(process.env.DB_HOST, parseInt(process.env.DB_PORT), process.env.DB_NAME);
 
-port = process.env.PORT || 3030;
+require('./config/development');
 
-//conection = mongoose.connection;
-conection1 = mongoose.createConnection(process.env.DB_HOST, parseInt(process.env.DB_PORT), process.env.DB_NAME);
-conection2 = mongoose.createConnection(process.env.DB_HOST, parseInt(process.env.DB_PORT), 'jspro');
+mongoose.connect('mongodb://localhost:27017/test');
 
-conection1.once('connected', function(db){
+conection = mongoose.connection;
+conection.once('connected', function(db){
     console.log('---— Connected —---');
 
-    function modifireMidlware(req, res, next){
+    var Schema = mongoose.Schema;
+    var useeSchema = new Schema({
+        _id: Number,
+        name:{
+            first: String,
+            last: String
+        },
+        dateOfBirth: {type: Date, default: Date.now},
+        friends: [{type: Number, ref: 'user'}],
+        age: Number
+    });
+  var User = mongoose.model('user', useeSchema);
+
+  function modifireMidlware(req, res, next){
         req.myVar = 'adfadf';
         next();
     };
 
     app.post('/user', function(req, res, next){
-        res.status(200).send(req.body);
+        var user = new User({name:'VasyPupkin', age:24});
+        user.save(function (eer, user) {
+            console.log(user.age);
+            
+        });
     });
-    app.use(modifireMidlware);
+   // app.use(modifireMidlware);
     app.get('/', function(req, res, next){
-        console.log(req.myVar);
-        res.status(200).send(req.ip);
+        User.find({_id: 5})
+            .populate('friends', {_id:0, friends:0})
+            .exec(function(err, users){
+            if(err){
+                return next(err);
+            }
+            res.status(200).send(users);
+        });
+
     });
 
     app.listen(port, function(){
         console.log('Server start succes =' + port);
-
     });
 });
-conection1.on('error', function(err){
+conection.on('error', function(err){
     console.error(err);
 
     process.exit(1);
 });
- conection2.once('connected', function(db){
+ /*conection2.once('connected', function(db){
   console.log('---— Connected2 —---');
 
   });
@@ -54,4 +74,4 @@ conection1.on('error', function(err){
   console.error(err);
 
   process.exit(1);
-});
+});*/
