84f1d7aa40b1535d27e51df2f0a659cc1dfac471
danil <danilavajda@gmail.com>
2017-04-01 22:41:00 +0300

    Schema and comments were added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3030;;
+var port = process.env.PORT || 3030;
 var mongoose = require ('mongoose');
 var conection;
 /*var conectin1;
@@ -13,24 +13,24 @@ var conection2;*/
 
 require('./config/development');
 
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://localhost:27017/test'); //подключаемся к БД
 
 conection = mongoose.connection;
-conection.once('connected', function(db){
-    console.log('---— Connected —---');
+conection.once('connected', function(db){   //Если подключились - пишет ---Connected---
+    console.log('---— Connected —---');   //
 
-    var Schema = mongoose.Schema;
-    var useeSchema = new Schema({
+    var Schema = mongoose.Schema; //
+    var useeSchema = new Schema({ // создаём схему
         _id: Number,
         name:{
             first: String,
             last: String
         },
         dateOfBirth: {type: Date, default: Date.now},
-        friends: [{type: Number, ref: 'user'}],
+        friends: [{type: Number, ref: 'user'}],  // в ref указывается название модели
         age: Number
     });
-  var User = mongoose.model('user', useeSchema);
+  var User = mongoose.model('user', useeSchema); // называем модель -> mongoose.model('название модели', название схемы)
 
   function modifireMidlware(req, res, next){
         req.myVar = 'adfadf';
@@ -46,22 +46,21 @@ conection.once('connected', function(db){
     });
    // app.use(modifireMidlware);
     app.get('/', function(req, res, next){
-        User.find({_id: 5})
+        User.find({_id: 5})                      // поиск в БД по _id
             .populate('friends', {_id:0, friends:0})
-            .exec(function(err, users){
+            .exec(function(err, users) {
             if(err){
-                return next(err);
+                return next(err);   // если есть ошибка - выписать то, что находится в ()
             }
             res.status(200).send(users);
         });
 
     });
 
-    app.listen(port, function(){
-        console.log('Server start succes =' + port);
+    app.listen(port, function(){                         //при запросе на port выполняется функция
+        console.log('Server start succes = ' + port);
     });
-});
-conection.on('error', function(err){
+});conection.on('error', function(err){ //если нету подключения - написать error
     console.error(err);
 
     process.exit(1);
